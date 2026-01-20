4a26934f818fada8956bd8859d9881e3c58d647a
Shreya Shristi <shristishreyac@gmail.com>
2026-01-20 23:32:07 +0530

    Sliding window

--- Training.java/Main11.java
+++ Training.java/Main11.java
@@ -1,5 +1,23 @@
 public class Main11 {
     public static void main(String[] args){
         System.out.println("hello");
+        int[] a={1,2,3,4,5,6,7,11,-1,0};
+        System.out.println("Minimum sum in the subarray of size k=3 is "+minSub(a,3));
+    }
+    static int minSub(int[] a,int k){
+        int sum=0;
+        for(int i=0;i<k;i++){
+            sum+=a[i];
+        }
+        int l=0;
+        int r=k;
+        int min=sum;
+        for(int i=k;i<a.length;i++){
+            sum+=a[r++]-a[l++];
+            if(sum<min){
+                min=sum;
+            }
+        }
+        return min;
     }
 }
