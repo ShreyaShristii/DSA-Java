3a4f25186a8f16164ef2f4a5059f057bc6c07f49
Shreya Shristi <shristishreyac@gmail.com>
2026-01-23 22:31:58 +0530

    LinkedList Practice

--- linkedlist/LLImplementation.java
+++ linkedlist/LLImplementation.java
@@ -11,9 +11,30 @@ public class LLImplementation {
         l1.insert(2);
         l1.insert(4);
         l1.insert(6);
+        l1.insert(7);
+        l1.insert(8);
+        l1.insert(9);
         l1.print();
         l1.head = l1.reverse(l1.head);
         l1.print();
+        l1.head=l1.reverse(l1.head);
+        System.out.print("Original Linked Lists was: ");
+        l1.print();
+        System.out.println(l1.mid(l1.head).data);
+        LinkedList a=new LinkedList();
+        a.insert(9);
+        a.insert(9);
+        a.insert(9);
+        LinkedList b=new LinkedList();
+        b.insert(9);
+        b.insert(9);
+        b.insert(9);
+        Node r=a.sum(a.head,b.head);
+        LinkedList res=new LinkedList();
+        res.head=r;
+        res.print();
+
+
     }
 }
 
@@ -66,5 +87,64 @@ class LinkedList {
         }
         return prev;
     }
+    Node reverse1(Node head){
+        if(head==null)return null;
+        Node n=null;
+        while(head!=null){
+            Node temp=head;
+            head=head.next;
+            temp.next=n;
+            n=temp;
+        }
+        return n;
+    }
+    Node mid(Node head){
+        if(head==null)return null;
+        if(head.next==null)return head;
+        Node fast=head;
+        Node slow=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow;
+    }
+    Node sum(Node headA, Node headB){
+        if(headA==null)return headB;
+        if(headB==null)return headA;
+        int c=0;
+       // int s=0;
+        Node t1=headA;
+        Node t2=headB;
+        t1=reverse(t1);
+        t2=reverse(t2);
+        Node result=null;
+        //Node temp=null;
+        while(t1!=null || t2!=null){
+            int s=c;
+            if(t1!=null){
+                s+=t1.data;
+                t1=t1.next;
+        }
+        if(t2!=null){
+            s+=t2.data;
+            t2=t2.next;
+        }
+        
+        
+            c=s/10;
+            s=s%10;
+        Node n=new Node(s);
+       n.next=result;
+       result=n;
+
+    }
+    if(c>0){
+        Node n=new Node(c);
+        n.next=result;
+        result=n;
+    }
+    return result;
+}
 }
 
